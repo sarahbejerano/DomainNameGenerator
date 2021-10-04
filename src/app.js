1a07ae5f282c6d91a72f1afc352bb0d09487a0c0
sarahbejerano <bejeranosarah@gmail.com>
2021-10-04 21:52:20 +0000

    resolve problem

--- src/app.js
+++ src/app.js
@@ -5,27 +5,31 @@
 //import "./assets/img/rigo-baby.jpg";
 //import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let domain = [".com", ".net", ".es"];
-
-window.onload = function domainGenerator(pronoun, adj, noun, domain) {
-  const pronounIndex = Math.floor(Math.random() * pronoun.length);
-  const adjIndex = Math.floor(Math.random() * adj.length);
-  const nounIndex = Math.floor(Math.random() * noun.length);
-  const domainIndex = Math.floor(Math.random() * domain.length);
-  return (
-    pronoun[pronounIndex] +
-    adj[adjIndex] +
-    noun[nounIndex] +
-    domain[domainIndex]
-  );
-};
-
-function displayDomain() {
-  var finaldomain = domainGenerator();
-  document.getElementById(finaldomain).innerHTML = "";
+function generateDomains() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".es"];
+  let allDomains = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          let fullDomain = pronoun[i] + adj[j] + noun[k] + domain[l];
+          allDomains.push(fullDomain);
+        }
+      }
+    }
+  }
+  return allDomains;
 }
 
-console.log(displayDomain);
+window.onload = () => {
+  var finalDomains = generateDomains();
+  var ul = document.getElementById("domainNames");
+  for (var i = 0; i < finalDomains.length; i++) {
+    var li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML = li.innerHTML + finalDomains[i];
+  }
+};
